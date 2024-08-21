d683fcd7961048700ef1b5fe2638576ea6d34428
rekha-2002 <165414795+rekha-2002@users.noreply.github.com>
2024-08-21 17:35:46 +0530

    Add files via upload

---
+++ destinations.js
@@ -0,0 +1,12 @@
+var destinations = [
+    {
+        "name": "Manali",
+        "description":"Nestled in between the snow-capped slopes of the Pir Panjal and the Dhauladhar ranges. Manali is one of the most popular hill stations in India,with jaw-dropping views, lush green forests, a perpetual fairy-tale - Manali has been blessed with extraordinary scenic beauty. From museums to temples, from quaint little hippie villages to bustling upscale streets, river adventures to trekking trails, Manali has every reason to be the tourist magnet it is, all year round. Manali is a high-altitude Himalayan resort town in India’s northern Himachal Pradesh state. It has a reputation as a backpacking center and honeymoon destination. It’s a gateway for skiing in the Solang Valley and trekking in Parvati Valley. It's also a jumping-off point for paragliding, rafting and mountaineering in the Pir Panjal mountains.",
+        "map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26997.521594303595!2d77.16961016069348!3d32.23950714205165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39048708163fd03f%3A0x8129a80ebe5076cd!2sManali%2C%20Himachal%20Pradesh!5e0!3m2!1sen!2sin!4v1583378996972!5m2!1sen!2sin"
+    },
+    {
+        "name": "Shimla",
+        "description":"Situated at a height of 2200m, Shimla is the capital and the largest city of Himachal Pradesh in India. Set amidst beautiful hills and mystical woods, Shimla has been a very popular hill-station among Indian families and honeymooners since the last 50 years. British loved this city so much that they made Shimla their summer capital in 1864 and used to rule the sub-continent from here whenever the temperature rose in the nearby plains. Shimla still retains its old world charm with beautiful colonial architecture, pedestrian-friendly Mall Road and beautiful churches. The weather is pleasant for most of the months with tourists flocking especially during the summer months. The winters are cold with some days of snow from mid-December till February end.",
+        "map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26997.521594303595!2d77.16961016069348!3d32.23950714205165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39048708163fd03f%3A0x8129a80ebe5076cd!2sManali%2C%20Himachal%20Pradesh!5e0!3m2!1sen!2sin!4v1583378996972!5m2!1sen!2sin"
+    }
+]
